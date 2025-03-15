5740e3cafb6d10466ddc3a65aa4746f2a1ef6e03
nadhirahzlkfl <nadhirahaidilzulkafli@gmail.com>
2025-03-15 22:27:15 +0800

    Update file

--- aikitchen_app.py
+++ aikitchen_app.py
@@ -8,6 +8,7 @@ from ultralytics import YOLO
 from PIL import Image
 import os
 
+st.cache_resource.clear()
 #%% constants
 BASE_API_URL = "https://3131-2001-e68-5456-4913-3954-57ac-f3bb-c3.ngrok-free.app"
 FLOW_ID = "ce816029-b06a-4b56-9ca4-77ef44bdc839"
